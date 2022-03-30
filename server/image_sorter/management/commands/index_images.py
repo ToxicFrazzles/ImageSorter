8c734a318677bb1acd7cc82771cdab982fb16bc9
Frazer <frazzles740@gmail.com>
2022-03-31 00:06:28 +0100

    Make the index_images command repeat
    It now repeats every 120 seconds.
    
    This should allow time for instances where there are no active source directories to not just infinite loop and use 100% CPU

--- server/image_sorter/management/commands/index_images.py
+++ server/image_sorter/management/commands/index_images.py
@@ -2,28 +2,31 @@ from django.core.management.base import BaseCommand, CommandError
 from image_sorter.models import SourceDirectory, Image as ImageModel
 from pathlib import Path
 from PIL import Image, UnidentifiedImageError
+from time import sleep
 
 
 class Command(BaseCommand):
     help = 'Browses each source directory looking for images to add to the database'
 
     def handle(self, *args, **options):
-        source_directories = SourceDirectory.objects.filter(active=True)
-        for dir in source_directories:
-            dir_path = Path(dir.file_path).resolve()
-            if not dir_path.is_dir():
-                dir.active = False
-                dir.save()
-                continue
-            for entity in dir_path.iterdir():
-                if not entity.is_file():
+        while True:
+            source_directories = SourceDirectory.objects.filter(active=True)
+            for dir in source_directories:
+                dir_path = Path(dir.file_path).resolve()
+                if not dir_path.is_dir():
+                    dir.active = False
+                    dir.save()
                     continue
-                try:
-                    img = Image.open(entity)
-                except UnidentifiedImageError:
-                    continue
-                image = ImageModel(file_path=f"{entity}")
-                try:
-                    image.save()
-                except Exception as e:
-                    print(e)
+                for entity in dir_path.iterdir():
+                    if not entity.is_file():
+                        continue
+                    try:
+                        img = Image.open(entity)
+                    except UnidentifiedImageError:
+                        continue
+                    image = ImageModel(file_path=f"{entity}")
+                    try:
+                        image.save()
+                    except Exception as e:
+                        print(e)
+            sleep(120)
