1c6d55513bab5eb5e6a2236e8e6d5cd071c87afb
Frazer <frazzles740@gmail.com>
2022-03-31 12:40:30 +0100

    Make the image tagging experience a bit nicer

--- server/image_sorter/views/tag_image.py
+++ server/image_sorter/views/tag_image.py
@@ -1,5 +1,8 @@
+import json
+
 from .login_required import LoginRequiredView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.http.response import JsonResponse
 from django import forms
 from ..models import TagGroup, Image, Tag
 
@@ -23,20 +26,39 @@ class TagImageView(LoginRequiredView):
         ctx = {
             "tag_group": tag_group,
             "the_image": the_image,
+            "tags": tag_group.tag_set.all().order_by('name'),
             "form": ImageTagForm(tag_group)
         }
         ctx['form'].fields['image_field'].initial = the_image.id
         return render(request, 'image_sorter/tag_image.html', context=ctx)
 
     def post(self, request, tag_group: TagGroup):
-        form = ImageTagForm(tag_group, data=request.POST)
-
-        if not form.is_valid():
-            return redirect('image_sorter:tag_image', tag_group)
-        image_id = form.cleaned_data['image_field']
-        image = Image.objects.get(id=image_id)
-        tag_id = form.cleaned_data['tag']
-        tag = Tag.objects.get(id=tag_id, group=tag_group)
+        post_data = json.loads(request.body)
+        image = Image.objects.get(id=post_data.get("image_id"))
+        if image.tags.filter(group=tag_group).count() > 0:
+            # Image already tagged for this tag group.
+            # Remove the tag so it can be replaced
+            image.tags.remove(*tag_group.tag_set.all())
+        tag = tag_group.tag_set.get(id=post_data.get("tag_id"))
         image.tags.add(tag)
         image.save()
-        return redirect('image_sorter:tag_image', tag_group)
+
+        next_image = Image.objects.exclude(tags__group=tag_group).order_by('?').first()
+        return JsonResponse({
+            "next_image": {
+                "id": next_image.id,
+                "url": reverse("image_sorter:media", args=(next_image,))
+            }
+        })
+
+        # form = ImageTagForm(tag_group, data=request.POST)
+        #
+        # if not form.is_valid():
+        #     return redirect('image_sorter:tag_image', tag_group)
+        # image_id = form.cleaned_data['image_field']
+        # image = Image.objects.get(id=image_id)
+        # tag_id = form.cleaned_data['tag']
+        # tag = Tag.objects.get(id=tag_id, group=tag_group)
+        # image.tags.add(tag)
+        # image.save()
+        # return redirect('image_sorter:tag_image', tag_group)
