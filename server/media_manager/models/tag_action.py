adeccf13e504fe132b4c19d1e720e5f6f64db766
Frazer <frazzles740@gmail.com>
2022-04-05 22:05:42 +0100

    Oversight on which keyword argument was correct for DateTimeField

--- server/media_manager/models/tag_action.py
+++ server/media_manager/models/tag_action.py
@@ -5,5 +5,5 @@ class TagAction(models.Model):
     tag = models.ForeignKey('Tag', on_delete=models.CASCADE)
     media_file = models.ForeignKey('MediaFile', on_delete=models.CASCADE)
     human_tagged = models.BooleanField()
-    tagged_date = models.DateTimeField(auto_created=True)
+    tagged_date = models.DateTimeField(auto_now_add=True)
     certainty = models.DecimalField(max_digits=6, decimal_places=3)
