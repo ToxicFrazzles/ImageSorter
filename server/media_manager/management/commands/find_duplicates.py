1afc97fa66bad8dce309ed0567afd317111ae28d
Frazer <frazzles740@gmail.com>
2022-04-18 19:38:46 +0100

    Eliminate flagging images as similar when they are identical
    Also should handle instances when the database is locked

--- server/media_manager/management/commands/find_duplicates.py
+++ server/media_manager/management/commands/find_duplicates.py
@@ -2,9 +2,18 @@ import numpy as np
 from django.core.management.base import BaseCommand, CommandError
 from media_manager.models import SourceDirectory, MediaFile, MediaTypeChoices, Setting
 from pathlib import Path
+from django.db.utils import OperationalError
 from PIL import Image, UnidentifiedImageError
 from time import sleep
 from imagehash import dhash
+
+
+def is_animated(image):
+    try:
+        image.seek(1)
+        return True
+    except EOFError:
+        return False
 
 
 class Command(BaseCommand):
@@ -16,23 +25,39 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         while True:
             print("Searching for duplicates")
-            for media_file in MediaFile.objects.filter(similar_to=None, media_type=0, diff_hash1=None, diff_hash2=None, diff_hash3=None, diff_hash4=None):
-                image = Image.open(media_file.file_path)
-                raw_im_hash = dhash(image, hash_size=16)
-                im_hash = np.packbits(np.array(raw_im_hash.hash).reshape(1, 16**2), axis=1).view(np.int64)[0]
+            try:
+                for media_file in MediaFile.objects.filter(similar_to=None, media_type=0, diff_hash1=None, diff_hash2=None, diff_hash3=None, diff_hash4=None):
+                    image = Image.open(media_file.file_path)
+                    raw_im_hash = dhash(image, hash_size=16)
+                    im_hash = np.packbits(np.array(raw_im_hash.hash).reshape(1, 16**2), axis=1).view(np.int64)[0]
 
-                similar_images = MediaFile.objects.filter(media_type=0, diff_hash1=im_hash[0], diff_hash2=im_hash[1], diff_hash3=im_hash[2], diff_hash4=im_hash[3])
+                    similar_images = MediaFile.objects.filter(media_type=0, diff_hash1=im_hash[0], diff_hash2=im_hash[1], diff_hash3=im_hash[2], diff_hash4=im_hash[3])
 
-                for similar_image in similar_images:
-                    media_file.similar_to.add(similar_image)
-
-                media_file.diff_hash1 = im_hash[0]
-                media_file.diff_hash2 = im_hash[1]
-                media_file.diff_hash3 = im_hash[2]
-                media_file.diff_hash4 = im_hash[3]
-                media_file.save()
-            if options['once']:
-                print("Done!")
-                break
+                    image_arr = np.array(image)
+                    should_save = True
+                    for similar_image in similar_images:
+                        similar_img = Image.open(similar_image)
+                        similar_img_arr = np.array(similar_img)
+                        diff = image_arr != similar_img_arr
+                        if diff:
+                            media_file.similar_to.add(similar_image)
+                        elif not is_animated(image) and not is_animated(similar_img):
+                            should_save = False
+                            media_file.delete()
+                            Path(media_file.file_path).unlink()
+                            break
+                    if not should_save:
+                        continue
+                    media_file.diff_hash1 = im_hash[0]
+                    media_file.diff_hash2 = im_hash[1]
+                    media_file.diff_hash3 = im_hash[2]
+                    media_file.diff_hash4 = im_hash[3]
+                    media_file.save()
+                if options['once']:
+                    print("Done!")
+                    break
+            except OperationalError:
+                sleep(5)
+                continue
             print("Sleeping. Zzzzzz")
             sleep(120)
