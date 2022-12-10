94035cd9d855b16d11776f35c7b86b3c7322be34
toxicfrazzles <frazzles740@gmail.com>
2022-12-10 20:38:32 +0000

    Attempt to make files behave appropriately
    In Firefox, right-clicking a file from the tagging page and then "Open Image in New Tab" would instead create a new tab but download the file.
    
    This is an attempt to have the file open in the tab instead. It should still be possible to download the file with "Save Image As..."

--- server/media_manager/views/media.py
+++ server/media_manager/views/media.py
@@ -1,3 +1,4 @@
+from ..models import MediaFile
 from .login_required import LoginRequiredView
 from django.views.static import serve
 from django.http.response import HttpResponse
@@ -6,11 +7,12 @@ from pathlib import Path
 
 
 class MediaView(LoginRequiredView):
-    def get(self, request, image):
+    def get(self, request, image: MediaFile):
         if settings.USE_XACCEL_FOR_MEDIA:
             file_path = Path(image.file_path).relative_to(settings.MEDIA_HOME_DIRECTORY)
             response = HttpResponse()
-            response["Content-Disposition"] = f"attachment; filename={file_path.name}"
+            response["Content-Type"] = image.mime_type
+            response["Content-Disposition"] = f"inline; filename={file_path.name}"
             response["X-Accel-Redirect"] = f"/protected/{file_path}"
             return response
 
